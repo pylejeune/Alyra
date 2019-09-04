c1f676702a6680260a31cf1cb7e12771795f222a
pylejeune <lejeune.py@gmail.com>
2019-09-04 17:52:54 +0200

    exo

--- chapitre_4/4.2.1/application.js
+++ chapitre_4/4.2.1/application.js
@@ -1,5 +1,7 @@
 
-provider = ethers.getDefaultProvider();
+//provider = ethers.getDefaultProvider();
+let currentProvider = new web3.providers.HttpProvider('http://localhost:7545');
+let provider = new ethers.providers.Web3Provider(currentProvider);
 
 provider.getBlockNumber().then((blockNumber) => {
     console.log("Bloc actuel : " + blockNumber);
@@ -11,4 +13,89 @@ provider.getGasPrice().then((gasPrice) => {
     console.log("Prix du gaz : " + gasPriceString);
     document.getElementById("gasPrice").innerHTML = gasPriceString
 });
+
+
+
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "url",
+				"type": "string"
+			}
+		],
+		"name": "produireHash",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "destinataire",
+				"type": "address"
+			},
+			{
+				"name": "valeur",
+				"type": "uint256"
+			}
+		],
+		"name": "transfert",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "cred",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "devoir",
+				"type": "bytes32"
+			}
+		],
+		"name": "remettre",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+let contratCredibilite = new ethers.Contract("0xB0A828b36F477ABBfD156E1E9075aEa8AAc09B08", abi, provider);
+console.log(contratCredibilite);
+
+let maCredibilite =  contratCredibilite.cred("0x5c60D7d3aB9245B111602D3091A2d610A7997f98");
 
